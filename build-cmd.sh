17790cd83c61e324c7818bbf1e1b897bb24e0d45
Rye Mutt <rye@alchemyviewer.org>
2020-11-07 07:26:52 -0500

    Fix up macos build scripts

--- build-cmd.sh
+++ build-cmd.sh
@@ -58,17 +58,66 @@ pushd "$HUNSPELL_SOURCE_DIR"
             cp "$relbitdir"{.dll,.lib,.pdb,.exp} "$stage/lib/release"
         ;;
         darwin*)
-            opts="-m$AUTOBUILD_ADDRSIZE -arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD_RELEASE"
-            export CFLAGS="$opts"
-            export CXXFLAGS="$opts"
-            export LDFLAGS="$opts"
-            ./configure --prefix="$stage"
-            make
-            make install
-            mkdir -p "$stage/lib/release"
-            mv "$stage/lib/"{*.a,*.dylib,*.alias} "$stage/lib/release"
+            # Setup osx sdk platform
+            SDKNAME="macosx10.15"
+            export SDKROOT=$(xcodebuild -version -sdk ${SDKNAME} Path)
+            export MACOSX_DEPLOYMENT_TARGET=10.13
+
+            # Setup build flags
+            ARCH_FLAGS="-arch x86_64"
+            SDK_FLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -isysroot ${SDKROOT}"
+            DEBUG_COMMON_FLAGS="$ARCH_FLAGS $SDK_FLAGS -Og -g -msse4.2 -fPIC -DPIC"
+            RELEASE_COMMON_FLAGS="$ARCH_FLAGS $SDK_FLAGS -Ofast -ffast-math -g -msse4.2 -fPIC -DPIC -fstack-protector-strong"
+            DEBUG_CFLAGS="$DEBUG_COMMON_FLAGS"
+            RELEASE_CFLAGS="$RELEASE_COMMON_FLAGS"
+            DEBUG_CXXFLAGS="$DEBUG_COMMON_FLAGS -std=c++17"
+            RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
+            DEBUG_CPPFLAGS="-DPIC"
+            RELEASE_CPPFLAGS="-DPIC"
+            DEBUG_LDFLAGS="$ARCH_FLAGS $SDK_FLAGS -Wl,-headerpad_max_install_names -Wl,-macos_version_min,$MACOSX_DEPLOYMENT_TARGET"
+            RELEASE_LDFLAGS="$ARCH_FLAGS $SDK_FLAGS -Wl,-headerpad_max_install_names -Wl,-macos_version_min,$MACOSX_DEPLOYMENT_TARGET"
+
+            JOBS=`sysctl -n hw.ncpu`
+
+            # force regenerate autoconf
+            autoreconf -fvi
+
+            mkdir -p "build_debug"
+            pushd "build_debug"
+                CFLAGS="$DEBUG_CFLAGS" CPPFLAGS="$DEBUG_CPPFLAGS" CXXFLAGS="$DEBUG_CXXFLAGS" LDFLAGS="$DEBUG_LDFLAGS" \
+                    ../configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/debug"
+                make -j$JOBS
+                make install DESTDIR="$stage"
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    make check
+                fi
+            popd
+
+            mkdir -p "build_release"
+            pushd "build_release"
+                CFLAGS="$RELEASE_CFLAGS" CPPFLAGS="$RELEASE_CPPFLAGS" CXXFLAGS="$RELEASE_CXXFLAGS" LDFLAGS="$RELEASE_LDFLAGS" \
+                    ../configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/release"
+                make -j$JOBS
+                make install DESTDIR="$stage"
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    make check
+                fi
+            popd
+
+            pushd "$stage/lib/debug"
+                fix_dylib_id libhunspell-1*.dylib
+                dsymutil libhunspell-*.*.*.dylib
+                strip -x -S libhunspell-*.*.*.dylib
+            popd
+
             pushd "$stage/lib/release"
-              fix_dylib_id libhunspell-*.dylib
+                fix_dylib_id libhunspell-1*.dylib
+                dsymutil libhunspell-*.*.*.dylib
+                strip -x -S libhunspell-*.*.*.dylib
             popd
         ;;
         linux*)
@@ -103,25 +152,31 @@ pushd "$HUNSPELL_SOURCE_DIR"
             # force regenerate autoconf
             autoreconf -fvi
 
-            CFLAGS="$DEBUG_CFLAGS" CPPFLAGS="$DEBUG_CPPFLAGS" CXXFLAGS="$DEBUG_CXXFLAGS" ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/debug"
-            make -j$JOBS
-            make install DESTDIR="$stage"
+            mkdir -p "build_debug"
+            pushd "build_debug"
+                CFLAGS="$DEBUG_CFLAGS" CPPFLAGS="$DEBUG_CPPFLAGS" CXXFLAGS="$DEBUG_CXXFLAGS" \
+                    ../configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/debug"
+                make -j$JOBS
+                make install DESTDIR="$stage"
 
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                make check
-            fi
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    make check
+                fi
+            popd
 
-            make distclean
+            mkdir -p "build_release"
+            pushd "build_release"
+                CFLAGS="$RELEASE_CFLAGS" CPPFLAGS="$RELEASE_CPPFLAGS" CXXFLAGS="$RELEASE_CXXFLAGS" \
+                    ../configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/release"
+                make -j$JOBS
+                make install DESTDIR="$stage"
 
-            CFLAGS="$RELEASE_CFLAGS" CPPFLAGS="$RELEASE_CPPFLAGS" CXXFLAGS="$RELEASE_CXXFLAGS" ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" --libdir="\${prefix}/lib/release"
-            make -j$JOBS
-            make install DESTDIR="$stage"
-
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                make check
-            fi
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    make check
+                fi
+            popd
         ;;
     esac
     mkdir -p "$stage/include/hunspell"
